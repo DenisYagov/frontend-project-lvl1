5a94a091c78f3604866d34facec3c8249c74efc5
Denis Yagov <denis.yagov@gmail.com>
2022-04-21 13:22:30 +0500

    removed bug in case 1 argument for isPrimeNumber()

--- src/games/brainPrimeCore.js
+++ src/games/brainPrimeCore.js
@@ -6,6 +6,11 @@ const maxOperandValue = 100; // maximum operand value
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 
 const isPrimeNumber = (opA) => {
+  // in case opA === 1 or 0 it will be the prime number
+  if (opA < 2) {
+    return true;
+  }
+  // loop of progressive finding of devision with zero reminder
   for (let i = 2; i < opA; i += 1) {
     if (opA % i === 0) {
       return false;
